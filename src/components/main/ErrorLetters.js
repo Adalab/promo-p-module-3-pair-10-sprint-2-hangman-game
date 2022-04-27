c8eef521e16f240da06e1c30f5189ac93b701b04
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-04-27 22:41:34 +0200

    add ErrorLetters.js component steps to follow

---
+++ src/components/main/ErrorLetters.js
@@ -0,0 +1,12 @@
+/*
+Pues bien, en este ejercicio debéis crear el componente ErrorLetters y mover dentro todo lo relativo a esta funcionalidad: el HTML, Sass, la función de JS, los estilos... Y como en el ejercicio anterior esta función también necesita recibir por props word y userLetters.
+*/
+
+/*  
+PASOS A SEGUIR
+Cread un componente llamado ErrorLetters.
+Moved la etiqueta <div className="error"> desde App a este nuevo componente.
+Este componente necesita recibir por props tanto word como userLetters para poder pintar los guiones y las letras.
+Moved la función renderErrorLetters también dentro del componente para hacer el cálculo de los guiones que hay que pintar.
+Y por último pero no menos importante, importad el fichero letters.scss, ya que este componente usa estos estilos. 
+*/
